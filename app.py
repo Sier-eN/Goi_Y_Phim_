8aaf9cd95b9af0a08049629c23507dbb54396e59
Nghia-Nguyen <nghianguyen262005@gmail.com>
2025-07-07 20:44:55 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import requests
 
+# Hàm để lấy poster của phim từ API The Movie Database (TMDB)
 def fetch_poster(phim_id):
     api_key = 'd8e72da2e901ad82afbc71d5c65d6eca' 
     url = f'https://api.themoviedb.org/3/movie/{phim_id}?api_key={api_key}'
@@ -11,6 +12,7 @@ def fetch_poster(phim_id):
     full_path = f"https://image.tmdb.org/t/p/w500{poster_path}"
     return full_path
 
+# Tải dữ liệu phim và ma trận tương đồng từ file pickle
 phim = pickle.load(open("phim_list.pkl",'rb'))
 similarity = pickle.load(open("similarity.pkl",'rb'))
 phim_list = phim['title'].values
@@ -18,6 +20,7 @@ phim_list = phim['title'].values
 st.header("Gợi Ý Phim")
 selectvalues = st.selectbox("Chọn Phim", phim_list)
 
+# Hàm để gợi ý phim dựa trên phim đã chọn
 def recommend(phim1):
     index = phim[phim['title']==phim1].index[0]
     distance = sorted(list(enumerate(similarity[index])),reverse=True, key=lambda vector:vector[1])
@@ -32,6 +35,7 @@ def recommend(phim1):
 if st.button("Đưa Ra Gợi Ý"):
     st.write("Dựa theo phim bạn chọn, đây là 5 gợi ý cho dành cho bạn:")
     ten_phim, phim_poster = recommend(selectvalues)
+    # Hiển thị tên phim và poster trong 5 cột
     col1,col2,col3,col4,col5 = st.columns(5)
     with col1:
         st.text(ten_phim[0])
